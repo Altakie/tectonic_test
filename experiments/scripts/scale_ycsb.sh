3a3b25c78695ef7634f9b85d1a54071e979b73b6
Altakie <artemlavrov@brandeis.edu>
2026-04-10 16:21:55 -0400

    Fix script

--- experiments/scripts/scale_ycsb.sh
+++ experiments/scripts/scale_ycsb.sh
@@ -1,8 +1,8 @@
 cd 1x
 for file in $(ls); do
   scale=1000
-  perl -i -pe "s/(record_count)([0-9]+)/\$1 . \$2*$scale/e" $file
-  perl -i -pe "s/(op_count)([0-9]+)/\$1 . \$2*$scale/e" $file
+  perl -i -pe "s/(record_count=)([0-9]+)/\$1 . \$2*$scale/e" $file
+  perl -i -pe "s/(op_count=)([0-9]+)/\$1 . \$2*$scale/e" $file
 done
 cd ..
 # Get all dirs with ls command
@@ -10,8 +10,8 @@ for scale in 10 100; do
   cp 1x/* ${scale}x
   cd ${scale}x
   for file in $(ls); do
-    perl -i -pe "s/(record_count)([0-9]+)/\$1 . \$2*$scale/e" $file
-    perl -i -pe "s/(op_count)([0-9]+)/\$1 . \$2*$scale/e" $file
+    perl -i -pe "s/(record_count=)([0-9]+)/\$1 . \$2*$scale/e" $file
+    perl -i -pe "s/(op_count=)([0-9]+)/\$1 . \$2*$scale/e" $file
   done
   cd ..
 done
