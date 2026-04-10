8ed7db60218484a132d69616bb1eb78549a842d9
Altakie <artemlavrov@brandeis.edu>
2026-04-10 16:01:11 -0400

    Fix script

--- experiments/scripts/scale_tectonic.sh
+++ experiments/scripts/scale_tectonic.sh
@@ -2,7 +2,8 @@
 for scale in 10 100; do
   cp 1x/* ${scale}x
   cd ${scale}x
-  for file in $(ls):
-    perl -i -pe "s/(\"op_count\": )([0-9]+)(,)/\$1 . \$2*$scale . \$3/e" file
+  for file in $(ls); do
+    perl -i -pe "s/(\"op_count\": )([0-9]+)(,)/\$1 . \$2*$scale . \$3/e" $file
+  done
   cd ..
 done
