be573d0988d90a4160e645dd170dc6e966731809
Altakie <artemlavrov@brandeis.edu>
2026-05-14 16:08:07 -0400

    New script

--- experiments/scripts/test_benchmarking_both_all_scales.sh
+++ experiments/scripts/test_benchmarking_both_all_scales.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./test_tectonic.sh 1
-./test_tectonic.sh 5
-./test_tectonic.sh 10
+./test_tectonic_new.sh 1
+./test_tectonic_new.sh 5
+./test_tectonic_new.sh 10
 
 ./test_ycsb.sh 1
 ./test_ycsb.sh 5
