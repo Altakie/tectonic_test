71cc7fa6378a80da91a7db40c5f2d5b663484b12
Altakie <artemlavrov@brandeis.edu>
2026-05-15 07:22:03 -0400

    tectonic generation script

--- experiments/scripts/test_tectonic_generation.sh
+++ experiments/scripts/test_tectonic_generation.sh
@@ -11,14 +11,18 @@ workloads=(
 time="/usr/bin/time"
 tectonic="$HOME/Tectonic/target/release/tectonic-cli"
 db_path="/tmp/tectonic-rocksdb"
-stats="$HOME/data/generation/tectonic/rocksdb/ycsb/1x"
 runs=5
-spec_path="$HOME/workload_specs/tectonic/1x/"
-time=/usr/bin/time
+# time=/usr/bin/time
+# stats="$HOME/data/benchmarking/tectonic/rocksdb/1x/ycsb"
+# spec_path="$HOME/workload_specs/tectonic/1x/"
+scale=$1
+# stats="$HOME/data/benchmarking/tectonic/rocksdb/${scale}x/ycsb"
+stats="$HOME/data/generation/tectonic/${scale}x/ycsb/"
+# spec_path="$HOME/workload_specs/tectonic/ycsb/${scale}x/"
 
 cd $HOME/Tectonic
 git switch no-marker-array
-cargo build --release rocksdb
+cargo build --release --features rocksdb
 
 for wl in "${workloads[@]}"; do
   for run in $(seq 1 $runs); do
@@ -26,16 +30,17 @@ for wl in "${workloads[@]}"; do
     log_path="$stats/$prefix.out"
     time_path="$stats/$prefix.time"
     cpu_log_path="$stats/$prefix.cpu"
-    workload_path="$stats/$prefix.wl"
+    wl_out_path="$stats/$prefix.wl"
 
     sudo sysctl -w vm.drop_caches=3
+    # rm -rf $db_path
 
     $time -v -o $time_path $tectonic generate \
-      -w $spec_path/$wl.spec.json -o $workload_path > >(tee $log_path) &
+      --ycsb $wl -s $scale -o $wl_out_path > >(tee $log_path) &
     pid="$!"
 
     sleep 0.1
-    child_pid=$(pgrep -P $pid tectonic)
+    child_pid=$(pgrep -P $pid tectonic-cli)
     while ! ps -p $child_pid >/dev/null 2>&1; do sleep 0.01; done
 
     pidstat 1 -h -r -u -p $child_pid >"$cpu_log_path" &
